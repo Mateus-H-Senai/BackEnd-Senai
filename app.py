1a6ffd623c1f5b7438f207845692e2d3334337ca
Mateus Henrique Oliveira dos Santos <ART0060877@aluno.firjansenaisesi.com.br>
2023-08-29 11:44:18 -0300

    V0.1.1 - alteração do arquivo python

---
+++ app.py
@@ -0,0 +1,8 @@
+def FuncaoA(Valor = 0, Valor2 = 0):
+    ValorFinal = Valor + Valor2
+    return ValorFinal
+    
+print(print(FuncaoA(4, 2)))
+
+if(print() == None):
+    print("Wtf. O que você quer com isso?")
